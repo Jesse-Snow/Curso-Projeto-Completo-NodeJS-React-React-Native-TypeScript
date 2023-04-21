bd33b3117bfdd3d916a4e4a6e449e23ee68635c0
--global <--global>
2023-04-21 17:34:10 -0300

    First App test

---
+++ first_app/src/App.js
@@ -0,0 +1,14 @@
+import Nome from './components/Nome.js'
+
+function App() {
+  return (
+     <div>
+      <h1>Welcome to React!</h1>
+      <Nome guerreiro="Conan" idade="30" />
+      <br/>
+      <Nome guerreiro="Argos" idade="??" />
+     </div>
+  );
+}
+
+export default App;
---
+++ first_app/src/components/Nome.js
@@ -0,0 +1,7 @@
+function Nome(props){
+    return (
+        <span>O nome é: {props.guerreiro} e idade é {props.idade}</span>
+    );
+}
+
+export default Nome;
