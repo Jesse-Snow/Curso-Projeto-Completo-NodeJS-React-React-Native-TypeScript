7a576d3fd99a69af0d73a28972791421262dbfb8
--global <--global>
2023-04-21 18:20:32 -0300

    Using hooks in a form to register a user

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -2,22 +2,44 @@ import Nome from './components/Nome.js'
 import {useState} from 'react'
 
 function App() {
-  const [meuNumero,setMeuNumero] = useState(1);
+  const [nome,setNome] = useState('');
+  const [idade,setIdade] = useState('');
+  const [email,setEmail] = useState('');
+  const [user,setUser] = useState({});
 
-  function handleClick(numero){
-    setMeuNumero(numero);
+  function handleSubmit(event){
+    event.preventDefault();
+
+    setUser({
+      name:nome,
+      age:idade,
+      email:email
+    });
+    alert('Usuário Cadastrado');
   }
 
   return (
      <div>
-      <h1>Welcome to React!</h1>
-      <Nome guerreiro="Conan" idade="30" />
-      <br/>
-      <span>Teste do useState Número: {meuNumero}</span>
-      <br/>
-      <button onClick={ () => handleClick("200")}>
-        Botão
-      </button>
+      <h1>Cadastro de usuário</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label>Nome:</label><br/>
+        <input value = {nome} onChange={(e) => setNome(e.target.value)} placeholder='Digite seu nome'></input><br/>
+
+        <label>Idade:</label><br/>
+        <input input value = {idade} onChange={(e) => setIdade(e.target.value)} placeholder='Digite sua idade'></input><br/>
+
+        <label>Email:</label><br/>
+        <input input value = {email} onChange={(e) => setEmail(e.target.value)} placeholder='Digite seu Email'></input><br/>
+
+        <input type='submit' value='Enviar'></input>
+      </form>
+
+      <div>
+        <span>Bem vindo {user.name}</span><br/>
+        <span>Idade: {user.age}</span><br/>
+        <span>Email: {user.email}</span><br/>
+      </div>
      </div>
   );
 }
