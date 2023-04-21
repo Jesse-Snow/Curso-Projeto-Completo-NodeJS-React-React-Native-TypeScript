b61a54b256a1a59e133210f4313ab5c48a50f465
--global <--global>
2023-04-21 18:02:18 -0300

    Using hooks in a button

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -1,12 +1,23 @@
 import Nome from './components/Nome.js'
+import {useState} from 'react'
 
 function App() {
+  const [meuNumero,setMeuNumero] = useState(1);
+
+  function handleClick(numero){
+    setMeuNumero(numero);
+  }
+
   return (
      <div>
       <h1>Welcome to React!</h1>
       <Nome guerreiro="Conan" idade="30" />
       <br/>
-      <Nome guerreiro="Argos" idade="??" />
+      <span>Teste do useState Número: {meuNumero}</span>
+      <br/>
+      <button onClick={ () => handleClick("200")}>
+        Botão
+      </button>
      </div>
   );
 }
