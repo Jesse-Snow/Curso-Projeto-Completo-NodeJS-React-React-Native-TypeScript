7b6cfb9566dae0603e23cb924a2d9d4168d09315
jesse-snow <jesse.neves@outlook.com>
2023-05-25 07:47:27 -0300

    Added comments

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -1,7 +1,9 @@
 import Nome from './components/Nome.js'
 import {useState} from 'react'
 
+// Componente App
 function App() {
+  // [nomeDoHook,acessorDoHook] = useState(valorPadrão/tipoDoHook);
   const [input,setInput] = useState('');
   const [tarefa,setTarefa] = useState([
     'Comer brocolis',
@@ -9,18 +11,23 @@ function App() {
   ]);
 
   function handleSubmit(event){
+    // Previnir do evento do form limpar o form
     event.preventDefault();
 
     setTarefa([...tarefa,input]);
     setInput('');
   }
 
+  // Retorno de JSX
   return (
      <div>
       <h1>Cadastro de usuário</h1>
 
       <form onSubmit={handleSubmit}>
         <label>Nome da Tarefa:</label><br/>
+
+        {/* O valor do input será o Hook input, e ao mudar/digitar no elemento input do html uma função irá inserir
+        o valor do input */}
         <input value = {input} onChange={(e) => setInput(e.target.value)} placeholder='Digite a tarefa'></input><br/>
         
         <input type='submit' value='Enviar'></input>
@@ -29,6 +36,7 @@ function App() {
 
       <div>
         <ul>
+          {/* Será percorrido o array tarefa para retornar uma lista com os valores do array */}
           {tarefa.map( algumaTarefa => (
             <li key={algumaTarefa}>{algumaTarefa}</li>
           ))}
