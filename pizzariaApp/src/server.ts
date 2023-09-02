9d1119bab7fa83fd2755e3fd38775dd95f87ac5b
jesse-snow <jesse.neves@outlook.com>
2023-09-02 15:03:56 -0300

    Added midlewares to handle errors and cross-origin

--- pizzariaApp/src/server.ts
+++ pizzariaApp/src/server.ts
@@ -1,8 +1,31 @@
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors'; // Recomendado usar como segundo import
+import cors from 'cors';
+
 import { router } from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(router);
+
+
+// Midleware para tratamento de erros.
+// Se faz uso dos quatro parâmetros (err,req,res,next)
+app.use((err: Error,req : Request, res:Response, next: NextFunction) => {
+
+    // Caso seja uma instância da Classe Error
+    if( err instanceof Error){
+        return res.status(500).json({
+            error: err.message
+        });
+    }
+
+    // Caso não seja uma instância da Classe Error
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error.'
+    })
+});
 
 app.listen(3333, ()=> { console.log('Server running at port: 3333')});
