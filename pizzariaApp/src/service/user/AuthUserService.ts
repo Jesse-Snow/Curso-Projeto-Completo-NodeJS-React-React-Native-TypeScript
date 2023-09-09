97d661143a4c03c311b898a4503145fb4832139b
jesse-snow <jesse.neves@outlook.com>
2023-09-09 10:28:15 -0300

    Using jwt to genetrate tokens for authentications

--- pizzariaApp/src/service/user/AuthUserService.ts
+++ pizzariaApp/src/service/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken"
 
 interface AuthRequest {
     email: string;
@@ -26,8 +27,34 @@ class AuthUserService {
             throw new Error('User/Password is incorrect.');
         }
 
+        // Se passou por validações acima, gerar token para o usuário
+        
+        // Função sign -> primeiro parâmetro: Payload, segundo parâmetro: secret key
+        // terceiro parâmetro: opções de quando irá expirar o token, subject ( whom the token refers to)
+        /* A secret key virá de uma variável de ambiente 
+           -- ( forma mais segura, pois .env por padrão 
+                não são commitadas para o repositório 
+                local. ) 
+        */
+       const token = sign( { 
+          name: user.name, 
+          email: user.email
+        }, 
+        // Usando a keyword 'as' para que o typescript considere que será uma string e não será undefined
+         process.env.JWT_SECRET as string, 
+         {
+            subject: user.id,
+            expiresIn: '30d'
+         }
+       ) 
 
-        return {status: 'ok'}
+
+        return {
+            id: user.id,
+            name: user.name, 
+            email: user.email,
+            token: token
+        }
     }
 }
 
