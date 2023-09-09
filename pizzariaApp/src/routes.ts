7db211cb1f8e2020ed4b853828b3010e5949083b
jesse-snow <jesse.neves@outlook.com>
2023-09-09 16:57:52 -0300

    Created Category and List Category routes, Initialized product route, service and controller

--- pizzariaApp/src/routes.ts
+++ pizzariaApp/src/routes.ts
@@ -5,15 +5,25 @@ import isAuthenticated from './middlewares/isAuthenticated';
 import { CreateUserController } from './controller/user/CreateUserController';
 import { AuthUserController } from './controller/user/AuthUserController';
 import { DetailUserController } from './controller/user/DetailUserController';
+import { CreateCategoryController } from './controller/category/CreateCategoryController';
+import { ListCategoryController } from './controller/category/ListCategoryController';
+import { CreateProductController } from './controller/product/CreateProductController';
 
 const router = Router();
 
 // User Routes
 router.post('/users', new CreateUserController().handle);
-
 // Infos - 1 ( Chamadas de função com e sem parênteses )
 router.post('/session', new AuthUserController().handle)
-
 router.get('/me', isAuthenticated ,new DetailUserController().handle)
+
+
+// Category Routes
+router.post('/category',isAuthenticated , new CreateCategoryController().handle);
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+// Product Routes
+router.post('/product', isAuthenticated, new CreateProductController().handle);
+
 
 export { router }; 
