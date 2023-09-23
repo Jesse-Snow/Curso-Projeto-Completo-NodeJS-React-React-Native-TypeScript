627b445c63ad50f6fb7cea26fea8f935023804d5
jesse-snow <jesse.neves@outlook.com>
2023-09-23 18:27:28 -0300

    Installed Sass Created global styles

--- pizzariaApp_nextJs/my-app/src/pages/_app.tsx
+++ pizzariaApp_nextJs/my-app/src/pages/_app.tsx
@@ -1,5 +1,9 @@
+// Estilo global
+import '../styles/globals.scss'
+
 import { AppProps } from 'next/app'
 
+// Componente que irá redenrizar as pages ( outros componentes )
 export default function App({ Component, pageProps } : AppProps) {
   return <Component {...pageProps} />
 }
--- pizzariaApp_nextJs/my-app/src/pages/index.tsx
+++ pizzariaApp_nextJs/my-app/src/pages/index.tsx
@@ -1,11 +1,11 @@
+import styles from '../styles/Home.module.css';
+
 export default function Home() {
   return (
-    <>
       <div>
         <h1>
           App Next Js
         </h1>
       </div>
-    </>
   )
 }
