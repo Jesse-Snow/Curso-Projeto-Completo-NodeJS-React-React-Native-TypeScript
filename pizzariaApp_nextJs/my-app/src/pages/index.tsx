bd07ee56fdb7e28c9b4ae8473e85c61f2eb9f6e9
jesse-snow <jesse.neves@outlook.com>
2023-09-25 12:20:11 -0300

    Creating Button Component Added react-icons

--- pizzariaApp_nextJs/my-app/src/pages/index.tsx
+++ pizzariaApp_nextJs/my-app/src/pages/index.tsx
@@ -6,26 +6,40 @@ import logo from '../../public/logo.svg'
 import styles from '../styles/home.module.scss';
 
 import { Input } from "../components/ui/Input";
+import { Button } from "../components/ui/Button"
+import { useState } from "react";
 
 export default function Home() {
+  const [loading,setLoading] = useState();
+
   return (
       <> {/* Fragment: Tag sem nome e sem estilização */}
       <Head>
         <title>Sujeito Pizza - Faça seu Login</title>
       </Head>
       <div className={styles.containerCenter}>
-        <Image src={logo} alt="Logo Sujeito Pizza"/>
-        <div className={styles.login}>
-          <Input 
-            type="text"
-            placeholder="Digite seu e-mail"
-          />
+        <form>    
+          <Image src={logo} alt="Logo Sujeito Pizza"/>
+          <div className={styles.login}>
+            <Input 
+              type="text"
+              placeholder="Digite seu e-mail"
+            />
 
-          <Input 
-            type="password"
-            placeholder="Digite sua senha"
-          />
-        </div>
+            <Input 
+              type="password"
+              placeholder="Digite sua senha"
+            />
+
+            <Button 
+              type="submit"
+              loading={true}
+            >
+              Acessar
+            </Button>
+
+          </div>
+        </form>
       </div>
       </>
   )
