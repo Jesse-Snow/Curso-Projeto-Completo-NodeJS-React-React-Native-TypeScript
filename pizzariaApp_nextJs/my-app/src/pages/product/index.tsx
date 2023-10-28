cdb054a0a48e3eeb140a64758802d04de3db95c5
jesse-snow <jesse.neves@outlook.com>
2023-10-28 02:19:02 -0300

    Creating Product page/coponent  - Added base form  - Added image input with preview

--- pizzariaApp_nextJs/my-app/src/pages/product/index.tsx
+++ pizzariaApp_nextJs/my-app/src/pages/product/index.tsx
@@ -2,10 +2,34 @@ import Head from 'next/head';
 import styles from './styles.module.scss';
 import { Header } from '../../components/Header'
 
+import { FiUpload } from 'react-icons/fi';
+
 import { canSSRAuth } from '../../utils/canSSRAuth';
+
+import { useState,ChangeEvent } from 'react';
 
 
 export default function Product(){
+    const [avatarUrl,setAvatarUrl] = useState(null);
+
+    function handleChange(e: ChangeEvent<HTMLInputElement>){
+
+      if(!e.target.files){ // Caso não haja arquivos selecionados
+        return;
+      }
+
+      const image = e.target.files[0]; // Selecionar apenas 1 arquivo selecionado
+
+      if(!image){
+        return;
+      }
+
+      if(image.type === 'image/jpeg' || image.type === 'image/png' || image.type === 'image/jpg'){
+        setAvatarUrl(URL.createObjectURL(image));
+      }
+    }
+
+
     return (
         <>
           <Head>
@@ -16,6 +40,27 @@ export default function Product(){
             <main className={styles.container}>
                 <h1>Novo Produto</h1>
                 <form className={styles.form}>
+
+                    <label className={styles.labelAvatar}>
+                      <span>
+                        <FiUpload size={30} color='#FFF'/>
+                      </span>
+
+                      <input type='file' accept='image/png, image/jpeg, image/jpg' onChange={handleChange}/>
+
+                      { avatarUrl && 
+                        ( <img 
+                          className={styles.preview}
+                          src={avatarUrl}
+                          alt='Foto do Produto'
+                          width={250}
+                          height={250}
+                          />
+                        )
+                      }
+                      
+                    </label>
+
                     <select>
                         <option>Alguma Categoria</option>
                         <option>Outra Categoria</option>
