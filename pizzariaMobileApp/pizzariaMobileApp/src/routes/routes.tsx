7438505cddd5159f05e82ed8c86c080f612cdcf7
jesse-snow <jesse.neves@outlook.com>
2024-01-05 20:00:55 -0300

    - Added loading to routes.tsx

--- pizzariaMobileApp/pizzariaMobileApp/src/routes/routes.tsx
+++ pizzariaMobileApp/pizzariaMobileApp/src/routes/routes.tsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { View,ActivityIndicator } from 'react-native';
 
 import AppRoutes from './app.routes';
 import AuthRoutes from './auth.routes';
@@ -7,6 +7,22 @@ import AuthRoutes from './auth.routes';
 
 function Routes(){
     const isAuthenticated = false;
+    const loading = true;
+
+    if(loading){
+        return (
+            <View
+              style={{
+                flex:1,
+                justifyContent:'center',
+                alignItems:'center',
+                backgroundColor:'#1D1D2E'
+              }}
+            >
+              <ActivityIndicator size={60} color='#FFF' />
+            </View>
+        )
+    }
 
     return (
        isAuthenticated ? <AppRoutes /> : <AuthRoutes />
