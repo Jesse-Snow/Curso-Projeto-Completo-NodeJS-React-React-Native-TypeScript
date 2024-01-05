e71d73835979ba453fc4f2743b672691715cc402
jesse-snow <jesse.neves@outlook.com>
2024-01-05 20:39:29 -0300

    - Controlling SignIn Component TextInputs and handling button Press

--- pizzariaMobileApp/pizzariaMobileApp/src/pages/SignIn/index.tsx
+++ pizzariaMobileApp/pizzariaMobileApp/src/pages/SignIn/index.tsx
@@ -1,6 +1,15 @@
-import { View, Text,StyleSheet,Image,TextInput,TouchableOpacity} from 'react-native';
+import { View, Text,StyleSheet,Image,TextInput,TouchableOpacity, Alert} from 'react-native';
+
+import { useState } from 'react';
 
 export default function SignIn(){
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    function handleButton(){
+        Alert.alert('email:' + email + ', senha:' + password);
+    }
+
     return (
         <View style={styles.container}>
             <Image style={styles.logo} source={require('../../assets/logo.png')}/>
@@ -10,6 +19,8 @@ export default function SignIn(){
                   placeholder='Digite seu email'
                   style={styles.input}
                   placeholderTextColor='#F0F0F0'
+                  onChangeText={(text) => setEmail(text)}
+                  value={email}
                 />
 
                 <TextInput 
@@ -17,9 +28,15 @@ export default function SignIn(){
                   style={styles.input}
                   placeholderTextColor='#F0F0F0'
                   secureTextEntry={true}
+                  onChangeText={(text) => setPassword(text)}
+                  // também pode ser onChangeTex(setPassword)
+                  value={password}
                 />
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity 
+                  style={styles.button}
+                  onPress={handleButton}
+                >
                     <Text style={styles.buttonText}>Acessar</Text>
                 </TouchableOpacity>
             </View>
