8cc8957ac96979e1e820598d9c984eefd61f267a
jesse-snow <jesse.neves@outlook.com>
2024-01-13 15:52:11 -0300

    - Structuring and Styling Dashboard page/component

--- pizzariaMobileApp/pizzariaMobileApp/src/pages/Dashboard/index.tsx
+++ pizzariaMobileApp/pizzariaMobileApp/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { View,Text,Button } from 'react-native';
+import { SafeAreaView,TouchableOpacity,Text,TextInput,StyleSheet } from 'react-native';
 import { useContext } from 'react';
 
 import { AuthContext } from '../../contexts/AuthContext';
@@ -7,9 +7,52 @@ export default function Dashboard(){
     const { signOut } = useContext(AuthContext);
 
     return (
-        <View>
-            <Text>Dashboard</Text>
-            <Button title='Click' onPress={signOut}></Button>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Novo Pedido</Text>
+            <TextInput style={styles.input} placeholder='Número da Mesa' keyboardType='numeric' placeholderTextColor='#F0F0F0' />
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Abrir Mesa</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        paddingVertical:15,
+        backgroundColor:'#1D1D2E'
+    },
+    title:{
+        fontSize:30,
+        fontWeight:'bold',
+        color:'#FFF',
+        marginBottom:24
+    },
+    input:{
+        width:'90%',
+        height:60,
+        backgroundColor: '#101026',
+        borderRadius:4,
+        paddingHorizontal:8,
+        textAlign:'center',
+        fontSize:22,
+        color:'#FFF'
+    },
+    button:{
+        width:'90%',
+        height:40,
+        backgroundColor:'#3FFFA3',
+        borderRadius:4,
+        marginVertical:12,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    buttonText:{
+        fontSize:18,
+        color:'#101026',
+        fontWeight:'bold'
+    }
+});
