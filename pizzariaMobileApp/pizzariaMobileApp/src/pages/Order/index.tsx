c1f24a67f1af0be8af9ef24fed21ab5efc67506a
jesse-snow <jesse.neves@outlook.com>
2024-01-21 10:26:38 -0300

    Consuming params of useNavigation in Order passed by Dashboard component

--- pizzariaMobileApp/pizzariaMobileApp/src/pages/Order/index.tsx
+++ pizzariaMobileApp/pizzariaMobileApp/src/pages/Order/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { View,Text,StyleSheet } from 'react-native';
 
+import { useRoute, RouteProp } from '@react-navigation/native';
+
+type RouteDetailParams = {
+    Order: {
+        orderNumber: string | number,
+        order_id: string
+    }
+};
+
+type OrderRouteProps = RouteProp<RouteDetailParams>;
+
 export default function Order(){
+    const route = useRoute<OrderRouteProps>();
     return(
         <View style={styles.container}>
             <Text>Tela Order</Text>
+            <Text>{route.params.orderNumber}</Text>
+            <Text>{route.params.order_id}</Text>
         </View>
     )
 }
